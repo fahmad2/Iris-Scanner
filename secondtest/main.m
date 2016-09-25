c5b9984bff61dd59bb2df3a025834559881d04b6
fahmad2 <faizanurfi@dhcp130.merunorth102.iit.edu>
2016-09-25 18:55:51 -0500

    commit main

--- secondtest/main.m
+++ secondtest/main.m
@@ -9,9 +9,22 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    SimpleCar *myCar = [[SimpleCar alloc] init];
+    
+    NSNumber *newVin = [NSNumber numberWithInt:123];
+    
+    [myCar setVin:newVin];
+    [myCar setMake:@"Honda" andModel:@"Civic"];
+    
+    NSLog(@"The car is: %@ %@", [myCar make], [myCar model]);
+    NSLog(@"The vin is: %@", [myCar vin]);
+    
+    [myCar release];
+    
+    [pool drain];
+    
     return 0;
 }
